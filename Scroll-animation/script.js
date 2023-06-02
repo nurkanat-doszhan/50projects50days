5600f7ed59235d1ce9bef277f08fe137fa5bc8e7
nurkanat-doszhan <nurkanat.doszhan@gmail.com>
2023-06-02 18:01:42 +0600

    Кое-что добавил

--- Scroll-animation/script.js
+++ Scroll-animation/script.js
@@ -1,6 +1,11 @@
 let content = document.getElementsByClassName('content')
+// window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
 
-console.log(window.innerHeight)
+
+window.onscroll = function () {
+  console.log(window.scrollY)
+    // console.log(document.body.scrollHeight)
+}
 for (let i = 0; i < content.length; i++) {
-  console.log(content[i].scrollLeft)
+  console.log(content[i].coor);
 }
